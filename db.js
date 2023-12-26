afc95ac80897cce0355b3ec575f91642718367d8
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-12-26 19:41:39 +0400

    add vercel.json

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + '?sslmode=required',
+  connectionString: process.env.POSTGRES_URL + '?sslmode=require',
+});
+
+pool.connect((err) => {
+  if (err) throw err;
+  console.log('Connect to postgress successfully');
 });
 
 module.exports = pool;
