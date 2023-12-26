b128a1e6f5e493ae1ba7619c68291a4e93689ca3
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-12-26 17:14:41 +0400

    change env variables

--- db.js
+++ db.js
@@ -3,11 +3,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  db_url: process.env.DB_URL,
 });
 
 module.exports = pool;
